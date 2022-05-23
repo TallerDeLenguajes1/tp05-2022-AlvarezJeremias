8e3d9168ec0420a31abc9eaddf7e45fd664ac14a
AlvarezJeremias <jeremiasal0209@gmail.com>
2022-05-23 16:31:04 -0300

    Primer ejecicio realizado, estoy probando las funciones de Git que nos permite usar VS 2022

---
+++ TP5/TP5/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+Console.Write("Escriba el numero a invertir: ");
+int numero, reverso=0, resto;
+
+numero=int.Parse(Console.ReadLine());
+
+while (numero>0){
+    resto = numero % 10;
+    reverso = reverso * 10 + resto;
+    numero = numero / 10;
+    
+}
+Console.Write("el numero ingresado invertido es : " + reverso);
+
